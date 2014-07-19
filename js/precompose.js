25c132073debd539509866242047ac0224c7d61b
Robert Moore <Ratchet201@yahoo.com>
2014-07-19 18:44:55 -0400

    rest

--- js/precompose.js
+++ js/precompose.js
@@ -80,9 +80,9 @@ function MakeRequest(divid,url,videoDiv) {
                         }, false);	
 	}
 	
-	player.addEvent('finish', onFinish);
+	player.addEvent('finish', onFinish);setupSimpleButtons();
 	});
-	setupSimpleButtons()
+	
 }
 function DemoReel(divid,url){
 		out='<iframe id=player1 src="http://player.vimeo.com/video/'+url+'?title=0&amp;byline=0&amp;portrait=0&amp;color=d9c787&amp;autoplay=0;player_id=player1" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'; 
